0d21950e99bd000763b3d22684486e95d41ff9ec
le-jour-j <151449840+le-jour-j@users.noreply.github.com>
2026-03-18 19:27:07 +0100

    banque fantome vercel v3

--- banque-fantome/src/main.jsx
+++ banque-fantome/src/main.jsx
@@ -4,16 +4,9 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
-// Récupère la route sauvegardée par 404.html
-const redirect = sessionStorage.getItem('redirect')
-if (redirect) {
-  sessionStorage.removeItem('redirect')
-  window.history.replaceState(null, '', '/banque-fantome' + redirect)
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/banque-fantome">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
