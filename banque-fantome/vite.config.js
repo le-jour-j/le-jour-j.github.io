0c130a90f67276065376fe7ae009af9daf064541
le-jour-j <151449840+le-jour-j@users.noreply.github.com>
2026-03-18 18:57:23 +0100

    Update vite.config.js

--- banque-fantome/vite.config.js
+++ banque-fantome/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/banque-fantome/', // ← CHANGE si ton repo s'appelle autrement
+  base: '/', // ← CHANGE si ton repo s'appelle autrement
 })
